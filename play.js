f34a17599b9009da56f023ca34e5be74bec4ec19
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-02 14:46:43 -0400

    Added termination event handler (ctrl + c)

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const connect = require('../snake-client/client');
 console.log('Connecting ...');
 connect();
+
+// Configure user input via stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', key => handleUserInput(key));
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+setupInput();
