7276dbf051e9017a5cec72dd8de39a386c93d014
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-02 14:09:52 -0400

    Refactored into play/client modules

--- play.js
+++ play.js
@@ -1,22 +1,3 @@
-const net = require('net');
-
-// Establishes connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  // Add event handler to appropriate loc....
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
-
+const connect = require('../snake-client/client');
 console.log('Connecting ...');
 connect();
-
