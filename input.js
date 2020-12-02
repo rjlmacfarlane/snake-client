9344cde64b30f9603115e2240a764f3e8d6f5819
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-02 15:04:29 -0400

    Split input handler to separate module

---
+++ input.js
@@ -0,0 +1,17 @@
+// Configure user input via stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on('data', key => handleUserInput(key));
+  stdin.resume();
+  return stdin;
+};
+setupInput();
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,21 +1,7 @@
-const connect = require('../snake-client/client');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-
-// Configure user input via stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on('data', key => handleUserInput(key));
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
 
 setupInput();
